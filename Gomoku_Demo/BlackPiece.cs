2f2793ba660e8b1853d3d9be04abd25a27855383
BRUCE.YANG <BRUCE.YANG@TWTY3NB1081>
2018-04-20 15:12:20 +0800

    add class piece

---
+++ Gomoku_Demo/BlackPiece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku_Demo
+{
+    class BlackPiece : Piece 
+    {
+        public BlackPiece(int x, int y) : base(x, y) 
+        {
+
+        }
+    }
+}
---
+++ Gomoku_Demo/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Gomoku_Demo
+{
+    class Piece : PictureBox 
+    {
+        public Piece(int x, int y)
+        {
+            this.BackColor = Color.Transparent;
+            this.Location = new Point(x, y);
+            this.Size = new Size(50, 50);
+        }
+    }
+}
